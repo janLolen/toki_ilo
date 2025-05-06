a68f0f7155f628a9f53e97a7d14edd3148db786c
jan Lolen <163869597+janLolen@users.noreply.github.com>
2025-05-06 22:18:20 +0200

    tidier

--- interpreter.js
+++ interpreter.js
@@ -3,14 +3,35 @@ let labels = {}
 let pc = 0
 let output = document.getElementById("output")
 
+const invalidDoubleCartouche = /(󱦐)(󱦐|󱦑)(󱦑)/
+const labelRe = /^(󱥫󱦐)(.+)(󱦑󱤡)$/
+const gotoRe = /^(󱥄󱥩󱥫󱦐)(.+)(󱦑)$/
+const printTextRe = /^(󱥄󱥠󱤉󱥬󿬂)(.+)$/
+const printNumberRe = /^(󱥄󱥠󱤉󱤽󿬂)(.+)$/
+const printVariableRe = /^(󱥄󱥠󱤉󱥓󱦐)(.+)(󱦑)$/
+const varDeclarationRe = /^(󱥓󱦐)(.+)(󱦑󱤧󱤬)$/
+const varDeclarationAssignmentRe = /^(󱥓󱦐)(.+)(󱦑󱤧󱤬󱤧󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)$/
+const incrementRe = /^(󱥄󱥌󱤉󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)(󱥩󱥓󱦐)([^\n]+)(󱦑)$/
+const decrementRe = /^(󱥄󱥶󱤉󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)(󱥧󱥓󱦐)([^\n]+)(󱦑)$/
+const numberRe = /[󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+/
+const digitRe = /[󱤂󱥳󱥮󱤭󱤼󿵩󱤄]/
+const equalNumberRe = /^(󱥓󱦐)(.+)(󱦑󱤧󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)(󱤡)$/
+
 // TODO: function int->nnp
+
+function isInvalid(line) {
+    let invalid = false
+    if(line.match(invalidDoubleCartouche)) invalid = true
+    if(invalid) error("invalid line")
+    return invalid
+}
 
 function ositelen(text) {
     output.innerHTML += `<p>> ${text}</p>`
 }
 
 function nnpSingleDigit(digit) {
-    if(!/[󱤂󱥳󱥮󱤭󱤼󿵩󱤄]/.test(digit)) return NaN
+    if(!digitRe.test(digit)) return NaN
     switch(digit) {
         case "󱤂": return 0;
         case "󱥳": return 1;
@@ -39,52 +60,56 @@ function warning(message) {
     console.log("WARNING! - "+message);
 }
 
+function error(message) {
+    console.log("ERROR! - "+message)
+}
+
 function log(message) {
     console.log(message)
 }
 
 function isLabel(line) {
-    return (/^󱥫󱦐[^\n]+󱦑󱤡$/).test(line);
+    return labelRe.test(line);
 }
 
 function isGoto(line) {
-    return (/^󱥄󱥩󱥫󱦐[^\n]+󱦑$/).test(line);
+    return gotoRe.test(line);
 }
 
 function isPrintText(line) {
-    return (/^󱥄󱥠󱤉󱥬󿬂[^\n]+$/).test(line);
+    return printTextRe.test(line);
 }
 
 function isPrintNumber(line) {
-    return (/^󱥄󱥠󱤉󱤽󿬂[^\n]+$/).test(line);
+    return printNumberRe.test(line);
 }
 
 function isPrintVariable(line) {
-    return (/^󱥄󱥠󱤉󱥓󱦐[^\n]+󱦑$/).test(line);
+    return printVariableRe.test(line);
 }
 
 function isVarDeclaration(line) {
-    return (/^󱥓󱦐[^\n]+󱦑󱤧󱤬$/).test(line)
+    return varDeclarationRe.test(line)
 }
 
 function isVarDeclarationWithAssignment(line) {
-    return (/^󱥓󱦐[^\n]+󱦑󱤧󱤬󱤧󱥣[󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+$/).test(line)
+    return varDeclarationAssignmentRe.test(line)
 }
 
 function isIncrement(line) {
-    return (/^󱥄󱥌󱤉󱥣[󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+󱥩󱥓󱦐[^\n]+󱦑$/).test(line)
+    return incrementRe.test(line)
 }
 
 function isDecrement(line) {
-    return (/^󱥄󱥶󱤉󱥣[󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+󱥧󱥓󱦐[^\n]+󱦑$/).test(line)
+    return decrementRe.test(line)
 }
 
-function isEqual(line) {
-    return (/^󱥓󱦐[^\n]+󱦑󱤧󱥣[󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+󱤡$/).test(line)
+function isEqualNumber(line) {
+    return equalNumberRe.test(line)
 }
 
 function addLabel(line, pc) {
-    let name = line.replace(/(󱥫󱦐)([^\n]+)(󱦑󱤡)/,"$2")
+    let name = line.match(labelRe)[2]
     if(labels[name] == undefined) {
         labels[name] = pc
         log(`adding label ${name} at line ${pc}`)
@@ -92,16 +117,34 @@ function addLabel(line, pc) {
     else if(labels[name] != pc) warning(`duplicate label ${name} at line ${labels[name]} and line ${pc}`)
 }
 
+function removeSpaces(line) {
+    // 󱥠󱥍󱤂󱤮󱤧󱤬󱤡󱥠󱥳󱤧󱤬󱥶󱥍󱥠󱤆
+    // 󱥁󱤧󱥶󱤉󱥠󱥍󱤂󱤮
+    // 󱥉󱤽󱤧󱥷󱥠󱤉󱥂󱤡󱥆󱤧󱥶󱤂󱤉󱥠󱥍󱤂󱤮
+    let split = line.match(/^(.*󿬂)(.*)$/)
+    let text = ""
+    if(split != null) {
+        line = split[1]
+        text = split[2]
+    }
+    line = line.replaceAll(/([ 　])/g, "").replace(/󱥬󱤑󱤡.*/, "").replace(/\/\/.*/, "")
+    line = line+text
+    return line
+}
+
 function runLine(lines) {
     log(`running line ${pc}`)
-    let line = lines[pc].replaceAll(/[ 　]/g, "").replace(/󱥬󱤑󱤡.*/, "").replace(/\/\/.*/, "")
+
+    if(isInvalid(lines[pc])) return
+
+    let line = removeSpaces(lines[pc])
 
     if(isLabel(line)){
-        addLabel(line)
+        addLabel(line, pc)
     }
 
     if(isGoto(line)){
-        let name = line.replace(/(󱥄󱥩󱥫󱦐)([^\n]+)(󱦑)/,"$2")
+        let name = line.match(gotoRe)[2]
         let newpc
         if(labels[name] == undefined) {
             log("looking for label "+name)
@@ -116,22 +159,26 @@ function runLine(lines) {
     }
 
     if(isPrintText(line)){
-        let text = line.replace(/(󱥄󱥠󱤉󱥬󿬂)([^\n]+)/, "$2")
+        let text = line.match(printTextRe)[2]
         ositelen(text)
+        log(`printing text ${text}`)
     }
 
     if(isPrintNumber(line)){
-        let text = nnpParser(line.replace(/(󱥄󱥠󱤉󱤽󿬂)([^\n]+)/, "$2"))
+        let text = nnpParser(line.match(printNumberRe)[2])
         ositelen(text)
+        log(`printing text ${text}`)
     }
 
     if(isPrintVariable(line)){
-        let name = line.replace(/^(󱥄󱥠󱤉󱥓󱦐)([^\n]+)(󱦑)$/, "$2")
+        let name = line.match(printVariableRe)[2]
+        if(vars[name]==undefined) {error("undefined variable"); return}
         ositelen(vars[name].value)
+        log(`printing the value of variable ${name}`)
     }
     
     if(isVarDeclaration(line)) {
-        let name = line.replace(/(󱥓󱦐)([^\n]+)(󱦑󱤧󱤬)/,"$2")
+        let name = line.match(varDeclarationRe)[2]
         log(`creating variable ${name}`)
         vars[name] = {
             value: 0,
@@ -140,8 +187,9 @@ function runLine(lines) {
     }
 
     if(isVarDeclarationWithAssignment(line)) {
-        let name = line.replace(/(󱥓󱦐)([^\n]+)(󱦑󱤧󱤬󱤧󱥣[󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)/,"$2")
-        let value = nnpParser(line.replace(/(󱥓󱦐)([^\n]+)(󱦑󱤧󱤬󱤧󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)/,"$4"))
+        let split = line.match(varDeclarationAssignmentRe)
+        let name = split[2]
+        let value = nnpParser(split[4])
         log(`creating variable ${name} with value ${value}`)
         vars[name] = {
             value: value,
@@ -150,20 +198,28 @@ function runLine(lines) {
     }
 
     if(isIncrement(line)) {
-        let name = line.replace(/^(󱥄󱥌󱤉󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)(󱥩󱥓󱦐)([^\n]+)(󱦑)$/,"$4")
-        let value = nnpParser(line.replace(/^(󱥄󱥌󱤉󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)(󱥩󱥓󱦐)([^\n]+)(󱦑)$/,"$2"))
+        let split = line.match(incrementRe)
+        let name = split[4]
+        let value = nnpParser(split[2])
+        if(vars[name]==undefined) {error("undefined variable"); return}
         vars[name].value += value
+        log(`incrementing ${name} by ${value}`)
     }
 
     if(isDecrement(line)) {
-        let name = line.replace(/^(󱥄󱥶󱤉󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)(󱥧󱥓󱦐)([^\n]+)(󱦑)$/,"$4")
-        let value = nnpParser(line.replace(/^(󱥄󱥶󱤉󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)(󱥧󱥓󱦐)([^\n]+)(󱦑)$/,"$2"))
+        let split = line.match(decrementRe)
+        let name = split[4]
+        let value = nnpParser(split[2])
+        if(vars[name]==undefined) {error("undefined variable"); return}
         vars[name].value -= value
+        log(`decrementing ${name} by ${value}`)
     }
 
-    if(isEqual(line)) {
-        let name = line.replace(/^(󱥓󱦐)([^\n]+)(󱦑󱤧󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)(󱤡)$/,"$2")
-        let value = nnpParser(line.replace(/^(󱥓󱦐)([^\n]+)(󱦑󱤧󱥣)([󱤂󱥳󱥮󱤭󱤼󿵩󱤄]+)(󱤡)$/,"$4"))
+    if(isEqualNumber(line)) {
+        let split = line.match(equalNumberRe)
+        let name = split[2]
+        let value = nnpParser(split[4])
+        if(vars[name]==undefined) {error("undefined variable"); return}
         if(vars[name].value != value) pc++
     }
 }
